391e261a0c229f37fc23a4b12fe4766e2b488ffa
Mateusz <mateusz.splawski96@gmail.com>
2020-04-16 17:26:52 +0200

    production update

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from "styled-components";
-
 export const GlobalStyle = createGlobalStyle`
 *{
     box-sizing:border-box;
     padding:0;
     margin:0;
     font-family: 'Roboto', sans-serif;
-    scroll-behavior:smooth;
 }
 body{
     background:rgba(130,130,130,.2);
